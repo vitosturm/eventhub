6524cb3469a6cfa8c6da9bdffb45a62ab9ddafce
vitosturm <sturmvito@gmail.com>
2026-04-27 13:20:31 +0200

    feat: improve layout/navbar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,9 +3,10 @@ import Navbar from "../components/Navbar";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
-      <main className="p-4">
+
+      <main className="flex-1 p-6 max-w-5xl mx-auto w-full">
         <Outlet />
       </main>
     </div>
